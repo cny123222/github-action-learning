ade9425d1a0dcb820b886176e1d8ae11be110e10
cny123222 <cny123222@sjtu.edu.cn>
2025-11-10 14:41:53 +0800

    test: add condition-coverage JUnit tests for Main.add (normal, zero, negative, overflow)

--- src/test/java/com/MainTest.java
+++ src/test/java/com/MainTest.java
@@ -6,11 +6,34 @@ import org.junit.jupiter.api.Test;
 
 class MainTest {
 
-	@Test
-	void test() {
-		Main main = new Main();
-		int result = main.add(1, 1);
-		assertEquals(1, result);
-	}
+    @Test
+    void testAddNormal() {
+        Main main = new Main();
+        assertEquals(2, main.add(1, 1), "1 + 1 should equal 2");
+    }
+
+    @Test
+    void testAddZero() {
+        Main main = new Main();
+        assertEquals(Integer.MAX_VALUE, main.add(Integer.MAX_VALUE, 0), "MAX + 0 should be MAX");
+    }
+
+    @Test
+    void testNegativeAThrows() {
+        Main main = new Main();
+        assertThrows(IllegalArgumentException.class, () -> main.add(-1, 5), "Negative a should throw IllegalArgumentException");
+    }
+
+    @Test
+    void testNegativeBThrows() {
+        Main main = new Main();
+        assertThrows(IllegalArgumentException.class, () -> main.add(5, -1), "Negative b should throw IllegalArgumentException");
+    }
+
+    @Test
+    void testOverflowThrows() {
+        Main main = new Main();
+        assertThrows(ArithmeticException.class, () -> main.add(Integer.MAX_VALUE, 1), "Adding 1 to MAX_VALUE should throw ArithmeticException due to overflow");
+    }
 
 }
